ab0695c5392419366b9900092059b27e4c228a73
PEDRO P <pedropanjon8745@gmail.com>
2025-04-01 19:30:48 -0500

    finish

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 import models.Person;
+import controllers.MangePeople;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Person person = new Person("Pedro",18);
-        System.out.println(person.getName());
+        // Person person = new Person("Pedro",18);
+        // System.out.println(person.getName());
+
+        MangePeople mG = new MangePeople();
     }
 }
--- src/controllers/MangePeople.java
+++ src/controllers/MangePeople.java
@@ -7,18 +7,22 @@ public class MangePeople {
     private ViewConsole vista;
     private BubbleSort sorting;
     private Person[] people;
+
     public MangePeople() {
         // Instanciar clases
         this.vista = new ViewConsole();
         this.sorting = new BubbleSort();
         // Instanciar variables
         this.people = new Person[5];
-        this.people[0]=new Person("Alice", 30);
-        this.people[1]=new Person("Bob",25);
-        this.people[2]=new Person("Charlie",35);
-        this.people[3]=new Person("David",20);
-        this.people[4]=new Person("Eve",28);
+        this.people[0] = new Person("Alice", 30);
+        this.people[1] = new Person("Bob", 25);
+        this.people[2] = new Person("Charlie", 35);
+        this.people[3] = new Person("David", 20);
+        this.people[4] = new Person("Eve", 28);
+
+        vista.showessage("clase ManagePeople CRETATE");
+        vista.printPeopleList(people);
+
     }
 
-    
 }
