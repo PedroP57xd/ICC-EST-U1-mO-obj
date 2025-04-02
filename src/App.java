cf2aaf9fe88307bedfd24e16b3adbf1ec6a71226
PEDRO P <pedropanjon8745@gmail.com>
2025-04-01 19:23:27 -0500

    controllers

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import models.Person;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Person person = new Person();
-        System.out.println(person);
+        Person person = new Person("Pedro",18);
+        System.out.println(person.getName());
     }
 }
--- src/models/Person.java
+++ src/models/Person.java
@@ -13,8 +13,26 @@ public class Person {
         this.age = edad;
     }
 
-    @Override
-    public String toString() {
-        return "Person [name: " + name + ", age: " + age + "]";
+    
+    
+    public String getName() {
+        return name;
     }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+        @Override
+        public String toString() {
+            return "Person [name: " + name + ", age: " + age + "]";
+        }
 }
